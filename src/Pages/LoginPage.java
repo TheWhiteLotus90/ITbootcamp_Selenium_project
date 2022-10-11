64e982ddae3063faccc5c0ec5ce19add0797ec69
Nevena Grujic <nevenagruks90@gmail.com>
2022-10-11 15:23:43 +0200

    Created project and pages

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage {
-	
+public class LoginPage {	
 	private WebDriver driver;
 	private WebDriverWait wait;
 	
---
+++ src/Pages/NavPage.java
@@ -0,0 +1,76 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NavPage {
+	private WebDriver driver;
+	private WebDriverWait wait;
+	
+	public NavPage(WebDriver driver, WebDriverWait wait) {
+		this.driver = driver;
+		this.wait = wait;
+	}
+	
+	public WebElement getHomeLink(){
+		return driver.findElement(By.className("btnHome"));
+	}
+	
+	public WebElement getAboutLink() {
+		return driver.findElement(By.className("btnAbout"));
+	}
+	
+	public WebElement getMyProfileLink() {
+		return driver.findElement(By.className("btnProfile"));
+	}
+	
+	public WebElement getAdminBtn() {
+		return driver.findElement(By.className("btnAdmin"));
+	}
+	
+	public WebElement getAdminCitiesLink() {
+		return driver.findElement(By.className("btnAdminCities"));
+	}
+	
+	public WebElement getAdminUsersLink () {
+		return driver.findElement(By.className("btnAdminUsers"));
+	}
+	
+	public WebElement getLoginBtn() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'btnLogin')][1]"));
+	}
+	
+	public WebElement getSignUpBtn() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'btnLogin')][2]"));
+	}
+	
+	public WebElement getLogoutBtn() {
+		return driver.findElement(By.className("btnLogout"));
+	}
+	
+	public WebElement getChooseALanguageBtn () {
+		return driver.findElement(By.className("btnLocaleActivation"));
+	}
+	
+	public WebElement getENLangBtn() {
+		return driver.findElement(By.className("btnEN"));
+	}
+	
+	public WebElement getESLangBtn() {
+		return driver.findElement(By.className("btnES"));
+	}
+	
+	public WebElement getFRLangBtn() {
+		return driver.findElement(By.className("btnFR"));
+	}
+	
+	public WebElement getCNLangBtn() {
+		return driver.findElement(By.className("btnCN"));
+	}
+	
+	public WebElement getHeaderFromWelcomePage() {
+		return driver.findElement(By.className("display-2"));
+	}
+}
